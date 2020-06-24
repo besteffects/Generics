e014f1ae209f7fcc7c6c136ca7c3ab425cef2d19
Vit <besteffects@gmail.com>
2020-06-23 22:49:54 -0400

    CircularBufferTest

--- src/main/java/_1_WhyGenerics/CircularBuffer.java
+++ src/main/java/_1_WhyGenerics/CircularBuffer.java
@@ -2,21 +2,36 @@ package _1_WhyGenerics;
 
 public class CircularBuffer {
     private Object[] buffer;
-    private int readCursor=0;
-    private int writeCursor =0;
+    private int readCursor = 0;
+    private int writeCursor = 0;
 
-    public CircularBuffer(int size){
-        buffer=new Object[size];
+    public CircularBuffer(int size) {
+        buffer = new Object[size];
     }
+
+    //try to write something into the buffer
     public boolean offer(int value) {
+        if (buffer[writeCursor] != null) {
+            return false;
+        }
+        buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
         return true;
     }
 
-    public Object poll(){
-        return null;
+    //try to read something out of the buffer\
+    //returns the value that we can read out, otherwise it returns null
+    public Object poll() {
+        final Object value = buffer[readCursor];
+        //if a value to read is available
+        if (value != null) {
+            buffer[readCursor] = null; //read a value and remove it from a buffer
+            readCursor = next(readCursor);
+        }
+        return value;
     }
 
-    public  int next(int index){
-        return (index+1)%buffer.length;
+    public int next(int index) {
+        return (index + 1) % buffer.length;
     }
 }
