19575fbe1f61138edb1d1cab377ca00589f86002
Vit <besteffects@gmail.com>
2020-06-24 22:09:15 -0400

    Maps

---
+++ src/main/java/_2_Collections/MapExamples.java
@@ -0,0 +1,35 @@
+package _2_Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExamples {
+    public static void main(String[] args) {
+        Person donDraper = new Person("Don Draper", 89);
+        Person peggyOlson = new Person("Peggy Olson", 65);
+        Person bertCooper = new Person("Bert Cooper", 100);
+
+        Map<String, Person> madMen = new HashMap<>();
+        madMen.put(donDraper.getName(), donDraper);
+        madMen.put(peggyOlson.getName(), peggyOlson);
+        madMen.put(bertCooper.getName(), bertCooper);
+
+        //System.out.println(madMen.get("Don Draper"));
+//print keys
+        for (String name : madMen.keySet()) {
+            System.out.println(name);
+        }
+//print values
+        for (Person person:madMen.values()) {
+            System.out.println(person);
+        }
+//print both keys and values TODO more practice with this type of map entry
+        for (Map.Entry<String,Person> entry: madMen.entrySet()){
+            System.out.println(entry);
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+//print all map
+        System.out.println(madMen);
+    }
+}
